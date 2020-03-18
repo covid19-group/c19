952b6ad5ad2cc437bd2cbc554f11dd77f168012e
Louis Arge <louislva@gmail.com>
2020-03-18 20:38:36 +0100

    corrected spelling + added repo to FAQ

--- content/faq.js
+++ content/faq.js
@@ -11,7 +11,7 @@ export default {
         question: 'Hvordan kommer man i kontakt med de projektansvarlige?',
         answer: `**Primær** Martin Permin [+1 (323) 529-7523](tel:+13235297523) / [martin@pelion.app](mailto:martin@pelion.app)
 
-**Tech** Holger Torup [+45 60 55 07 09](tel:+4560550709) / [holger@pelion.app](mailto:holger@pelion.app])
+**Tech** Holger Thorup [+45 60 55 07 09](tel:+4560550709) / [holger@pelion.app](mailto:holger@pelion.app])
 
 **Kommunikation** Carl Kronika [+45 20 87 28 89](tel:+4520872789) / [ck@copus.dk](mailto:ck@copus.dk)
 `,
@@ -24,7 +24,12 @@ export default {
       {
         question: 'Hvorfor være frivillig?',
         answer:
-          'I Frankring laver parfume-fabrikkerne håndsprit. I England laver bilfabrikanterne respiratorer. I krisetid må vi alle hjælpe med det vi er gode til — og vi er gode til data!\n\n[Skriv dig op til at bidrage](https://airtable.com/shrM3SHGwxEnnCChj).',
+          'I Frankrig laver parfume-fabrikkerne håndsprit. I England laver bilfabrikanterne respiratorer. I krisetid må vi alle hjælpe med det vi er gode til — og vi er gode til data!\n\n[Skriv dig op til at bidrage](https://airtable.com/shrM3SHGwxEnnCChj).',
+      },
+      {
+        question: 'Er det open source?',
+        answer:
+          'Ja! Koden er frit tilgængelig på GitHub: [https://github.com/covid19-group/c19](https://github.com/covid19-group/c19)',
       },
     ],
   },
@@ -39,7 +44,7 @@ export default {
         question: 'How to get into contact with the people driving the project?',
         answer: `**Primary** Martin Permin [+1 (323) 529-7523](tel:+13235297523) / [martin@pelion.app](mailto:martin@pelion.app)
 
-**Tech** Holger Torup [+45 60 55 07 09](tel:+4560550709) / [holger@pelion.app](mailto:holger@pelion.app])
+**Tech** Holger Thorup [+45 60 55 07 09](tel:+4560550709) / [holger@pelion.app](mailto:holger@pelion.app])
 
 **Communication** Carl Kronika [+45 20 87 28 89](tel:+4520872789) / [ck@copus.dk](mailto:ck@copus.dk)
 `,
@@ -47,12 +52,17 @@ export default {
       {
         question: 'Why c19.dk?',
         answer:
-          'The "Flatten the curve" strategy means that we have little information about the number of Danes with symptoms who are not in contact with the health services. This tool reveals the dark figures, for the authorities to predict the pressure on hospitals better.',
+          'The "Flatten the curve" strategy means that we have little information about the number of Danes with symptoms who are not in contact with the health services. This tool reveals the number of unreported cases, to help the authorities predict the pressure on hospitals.',
       },
       {
         question: 'Why volunteer?',
         answer:
-          "In France, perfume factories produce hand sanitizer. In United Kingdom, the car manufacturers produce respirators. In time of crisis we must all contribute with the things we're good at — and we're good with data!\n\n[Sign up to contribute](https://airtable.com/shrM3SHGwxEnnCChj).",
+          "In France, perfume factories produce hand sanitizer. In the United Kingdom, the car manufacturers produce respirators. In time of crisis we must all contribute with the things we're good at — and we're good with data!\n\n[Sign up to contribute](https://airtable.com/shrM3SHGwxEnnCChj).",
+      },
+      {
+        question: 'Is this open source?',
+        answer:
+          "Yes! We're hosting the code on GitHub: [https://github.com/covid19-group/c19](https://github.com/covid19-group/c19)",
       },
     ],
   },
