9f96f1a6cb23a2e427f24e0ab8f61fc51891ab9f
holgerthorup <holgerthorup@gmail.com>
2020-03-19 18:23:56 +0100

    added consent doc in Danish

---
+++ content/consent.js
@@ -0,0 +1,75 @@
+export default {
+  'da-DK': `
+# C19.DK samtykkeerklæring
+
+## Samtykke
+Ved at acceptere denne samtykkeerklæring, giver du samtykke til at C19.DK må behandle personoplysninger om dig i
+overensstemmelse med betingelserne i denne samtykkeerklæring.
+
+## Hvorfor skal du afgive samtykke?
+Efter databeskyttelsesloven og databeskyttelsesforordningen (GDPR) kræves det, at du som bruger på C19.DK accepterer,
+at C19.DK må behandle de personoplysninger, herunder personfølsomme oplysninger, du afgiver til C19.DK. Hvis du ønsker
+at bruge tjenesterne på og via c19.dk skal du derfor afgive dit samtykke.
+
+## Formål
+Formålet med behandlingen af data er at give offentligheden, særligt myndighedernes sundhedsberedskab, et overblik over
+symptomer i den brede befolkning og dermed at give mulighed for at myndighederne kan agere på de helbredsoplysninger du
+indsender i forbindelse med bekæmpelse af COVID-19.
+
+## Hvilke personoplysninger?
+Du giver samtykke til, at C19.DK må behandle følgende personoplysninger om dig:
+
+-	Telefonnummer, postnummer, fødselsdato, køn og antallet af personer i din husstand, heraf børn under 18 år (dine ”basisoplysninger”)
+-	Helbredsoplysninger navnlig af relevans og i relation til COVID-19.
+
+Alle oplysningerne afgives frivilligt af dig.
+
+C19.DK behandler alle data krypteret og under iagttagelse af passende organisatoriske og tekniske foranstaltninger.
+
+## Vores behandling af data
+
+### Registrering af basisoplysninger
+Dine basisoplysninger, bliver registreret i C19.DK’s database. Dette er nødvendigt for at kunne indsamle dine
+opdaterede helbredsoplysninger løbende. Vi beder dig separat om et samtykke til at sende push-beskeder via SMS til dig,
+så vi sikrer løbende opdatering af dine data.
+
+### Udveksling af oplysninger med andre systemer
+C19.DK videregiver ikke dine oplysninger til andre systemer.
+
+### Visualisering af anonymiseret data
+Vi visualiserer dine anonymiserede data på c19.dk/data bl.a. med det formål at kunne give offentligheden, herunder
+særligt sundhedsmyndighederne, mulighed for at reagere på helbredsoplysningerne i forbindelse med bekæmpelse af
+COVID-19.
+
+Såfremt en selektion på c19.dk/data resulterer i mindre end 30 datapunkter, fremvises data ikke, således at det sikres,
+at det ikke er muligt for tredjeparter at identificere dig ud fra de anonymiserede data vi offentliggør.
+
+
+### Adgang til data
+Det er udelukkende den dataansvarlige, C19.DK der har adgang til ikke anonymiseret data. Anonymiseret
+data vil blive visualiseret på c19.dk/data som beskrevet ovenfor.
+
+### Opbevaring af data
+Medmindre opbevaring af data er påkrævet efter gældende lovgivning, opbevarer vi alene dine oplysninger så længe det er
+rimeligt nødvendigt for at opfylde det formål som oplysningerne er indsamlet til. Dine oplysninger vil blive
+pseudonymiseret så snart det ikke længere er nødvendigt at opbevare dine data.
+
+## Dine muligheder og rettigheder
+Du kan til hver en tid trække dit samtykke tilbage ved at kontakte C19.DK (v/ Holger Thorup, [holger@pelion.app](mailto:holger@pelion.app), [+4560550709](tel:+4560550709)). Hvis
+du trækker dit samtykke tilbage, vil dine data ikke længere blive fremvist på c19.dk.
+
+Du har ret til at få at vide, hvilke oplysninger om dig, der behandles på c19.dk og hvorfor. Du har også mulighed for
+at rette, begrænse eller bede os om at slette dine data ved at kontakte C19.DK (v/ Holger Thorup, [holger@pelion.app](mailto:holger@pelion.app), [+4560550709](tel:+4560550709)).
+
+Du har også ret til at klage over vores behandling af persondata til Datatilsynet, Carl Jacobsens Vej 35, 2500 Valby,
+læse mere her: https://www.datatilsynet.dk/generelt-om-databeskyttelse/klage-til-datatilsynet/.
+`,
+  'en-UK': `
+# C19.DK Decleration of Consent
+
+The decleration of consent is being translated.
+
+Please refer to the decleration of consent in Danish by changing the
+language setting for this site.
+`,
+};
