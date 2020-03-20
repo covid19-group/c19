0a87d731431b166d07be99d80d34f920ecfb17ee
martinpermin <martinpermin@gmail.com>
2020-03-20 13:01:33 +0100

    Adding a period.

--- content/index.js
+++ content/index.js
@@ -5,7 +5,7 @@ export default {
       titleBlue: 'Selvrapportering',
       subtitle:
         'Tilmeld dig og vær med til at kaste lys over Corona-mørketallet. Du modtager en SMS dagligt med et kort spørgeskema om din helbredsstatus. Sammen hjælper vi sundhedsvæsenet til mere repræsentative data.',
-      action: 'Meld dig til, selvom du er rask',
+      action: 'Meld dig til, selvom du er rask.',
       share: 'Del via: ',
       placeholder: 'Indtast telefonnummer',
       privacy: {
