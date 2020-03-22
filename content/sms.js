5d911738223f2c5dfbf3e7ea10b4cc4b46c65b1f
Louis Arge <louislva@gmail.com>
2020-03-22 12:25:08 +0100

    correction of copy so link works on iOS

--- content/sms.js
+++ content/sms.js
@@ -4,13 +4,13 @@ export default {
     followUpSurvey: 'Følg op med din status på c19.dk/',
     unsubscribe: 'Afmeld: c19.dk/u',
     activatePeers:
-      'C19.DK er klar, men før vi går i luften, har vi brug for din hjælp. \nFortæl dine nærmeste, at de skal tilmelde sig. Flere tilmeldte = bedre data.',
+      'c19.dk er klar, men før vi går i luften, har vi brug for din hjælp. \n\nFortæl dine nærmeste, at de skal tilmelde sig. Flere tilmeldte = bedre data.',
   },
   'en-UK': {
     authCode: 'Your c19.dk verification code is: ',
     followUpSurvey: 'Follow up with your status on c19.dk/',
     unsubscribe: 'Unsubscribe: c19.dk/u',
     activatePeers:
-      'C19.DK er klar, men før vi går i luften, har vi brug for din hjælp. \nFortæl dine nærmeste, at de skal tilmelde sig. Flere tilmeldte = bedre data.',
+      'c19.dk er klar, men før vi går i luften, har vi brug for din hjælp. \n\nFortæl dine nærmeste, at de skal tilmelde sig. Flere tilmeldte = bedre data.',
   },
 };
