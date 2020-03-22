8bb35d40f7c79498969ce4e82968bee2bb4e9cc8
Louis Arge <louislva@gmail.com>
2020-03-22 12:03:40 +0100

    copy correction

--- content/sms.js
+++ content/sms.js
@@ -4,13 +4,13 @@ export default {
     followUpSurvey: 'Følg op med din status på c19.dk/',
     unsubscribe: 'Afmeld: c19.dk/u',
     activatePeers:
-      'In order to get the best picture of the spread of COVID-19 in Denmark, we need more data. Feel free to share C19.dk with friends.', // TODO: copy; maybe even some sort of shorthand facebook share link?
+      'C19.DK er klar, men før vi går i luften, har vi brug for din hjælp. \nFortæl dine nærmeste, at de skal tilmelde sig. Flere tilmeldte = bedre data.',
   },
   'en-UK': {
     authCode: 'Your c19.dk verification code is: ',
     followUpSurvey: 'Follow up with your status on c19.dk/',
     unsubscribe: 'Unsubscribe: c19.dk/u',
     activatePeers:
-      'In order to get the best picture of the spread of COVID-19 in Denmark, we need more data. Feel free to share C19.dk with friends.', // TODO: copy; maybe even some sort of shorthand facebook share link?
+      'C19.DK er klar, men før vi går i luften, har vi brug for din hjælp. \nFortæl dine nærmeste, at de skal tilmelde sig. Flere tilmeldte = bedre data.',
   },
 };
