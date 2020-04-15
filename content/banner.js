0397814d69addead7138c46c88b0d8c03da63566
holgerthorup <holgerthorup@gmail.com>
2020-04-15 10:09:26 +0200

    DK language/grammer by Steen Svanholm

--- content/banner.js
+++ content/banner.js
@@ -1,4 +1,4 @@
 export default {
-  'da-DK': 'Meld dig til nu. C19.DK går i luften så snart vi får godkendelse fra de relevante myndigheder.',
-  'en-UK': 'Sign up now. C19.DK will go live as soon as we obtain an approval from relevant authorities.',
+  'da-DK': 'Meld dig til nu. c19.dk går i luften, så snart vi får godkendelse fra de relevante myndigheder.',
+  'en-UK': 'Sign up now. c19.dk will go live as soon as we obtain an approval from relevant authorities.',
 };
